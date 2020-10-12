b647bc4c9d1b078f5f9b3647c98ac13b5195a8e3
Zer0Stra1n <zero@DESKTOP-QPEQQVR.localdomain>
2020-10-11 22:19:23 -0400

    TS implementation for demo purposes

--- handler.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const awsServerless = require('aws-serverless-express');
-const app = require('./src/main');
-
-const server = awsServerless.createServer(app);
-
-exports.api = (event, context) => {
-  return awsServerless.proxy(server, event, context);
-};
