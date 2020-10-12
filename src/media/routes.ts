31d4c2534bd5c40f122249ae3db6467e3741af0d
Seth <Seth.Sandor@thehartford.com>
2020-10-12 09:14:34 -0400

    update with new data and tsconfig

--- src/media/routes.ts
+++ src/media/routes.ts
@@ -1,12 +1,13 @@
 'use strict';
 import { Router } from 'express';
+import fakeData from './movie_fake.json';
 
 const routes = Router({
     mergeParams: true
 });
 
 routes.get('/', (req, res) => {
-    res.status(200).json({hello: 'world'});
+    res.status(200).json(fakeData);
 });
 
 export default routes;
