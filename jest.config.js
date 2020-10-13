751a671e4ccda0f784be4243600282a4fc8110eb
Seth <Seth.Sandor@thehartford.com>
2020-10-13 15:04:51 -0400

    lint the jest config

--- jest.config.js
+++ jest.config.js
@@ -1,36 +1,42 @@
 const SRC_PATH = '<rootDir>/src';
 
 module.exports = {
-    clearMocks: true,
-    collectCoverage: true,
-    coverageDirectory: 'test/coverage',
-    coverageReporters: ['lcov','text-summary'],
-    coverageThreshold: {
-      global: {
-        statements: 70,
-        lines: 80,
-        branches: 50,
-        functions: 50
-      }
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'test/coverage',
+  coverageReporters: ['lcov', 'text-summary'],
+  coverageThreshold: {
+    global: {
+      statements: 70,
+      lines: 80,
+      branches: 50,
+      functions: 50,
     },
-    verbose: false,
-    moduleDirectories: [
-      "node_modules",
-      "src"
+  },
+  verbose: false,
+  moduleDirectories: [
+    'node_modules',
+    'src',
+  ],
+  moduleFileExtensions: [
+    'ts',
+    'json',
+    'js',
+  ],
+  roots: [SRC_PATH],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.spec.json',
+    },
+  },
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {outputDirectory: 'test/coverage/junit', outputName: 'test.xml'},
     ],
-    moduleFileExtensions: [
-      'ts',
-      'json',
-      'js'
-    ],
-    roots: [SRC_PATH],
-    globals: {
-      'ts-jest': {
-        tsConfig: 'tsconfig.spec.json'
-      }
-    },
-    reporters: ['default', ['jest-junit', {outputDirectory: 'test/coverage/junit', outputName: 'test.xml'}]],
-    transform: {
-      '^.+\\.(ts|js)$': 'ts-jest',
-    },
-  };
+  ],
+  transform: {
+    '^.+\\.(ts|js)$': 'ts-jest',
+  },
+};
