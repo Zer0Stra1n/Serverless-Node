cd1cf58678c3040b395f13930329b27cddb64c0d
Seth <Seth.Sandor@thehartford.com>
2020-10-13 11:12:57 -0400

    unit testing added

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+const SRC_PATH = '<rootDir>/src';
+
+module.exports = {
+    clearMocks: true,
+    collectCoverage: true,
+    coverageDirectory: 'test/coverage',
+    coverageReporters: ['lcov','text-summary'],
+    coverageThreshold: {
+      global: {
+        statements: 70,
+        lines: 80,
+        branches: 50,
+        functions: 50
+      }
+    },
+    verbose: false,
+    moduleDirectories: [
+      "node_modules",
+      "src"
+    ],
+    moduleFileExtensions: [
+      'ts',
+      'json',
+      'js'
+    ],
+    roots: [SRC_PATH],
+    reporters: ['default', ['jest-junit', {outputDirectory: 'test/coverage/junit', outputName: 'test.xml'}]],
+    transform: {
+      '^.+\\.(ts|js)$': 'ts-jest',
+    },
+  };
---
+++ src/media/media.service.spec.ts
@@ -0,0 +1,13 @@
+import { MediaService } from './media.service';
+import fakeData from './movie_fake.json';
+
+describe('MediaService', () => {
+    const service = new MediaService();
+
+    describe('getFakeData', () => {
+        it('should return the fake data from JSON', () => {
+            const result = service.getFakeData();
+            expect(result).toEqual(fakeData);
+        });
+    });
+});
