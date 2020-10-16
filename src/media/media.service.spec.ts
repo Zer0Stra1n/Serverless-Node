b0632a47df8a6e7a71e44ed844e014701b801500
Seth <Seth.Sandor@thehartford.com>
2020-10-16 09:12:40 -0400

    updated unit tests

--- src/media/media.service.spec.ts
+++ src/media/media.service.spec.ts
@@ -1,13 +1,60 @@
 import MediaService from './media.service';
-import fakeData from './movie_fake.json';
 
 describe('MediaService', () => {
   const service = new MediaService();
+  const fakeRecords = [{
+    title: 'hello',
+    poster: 'some_url.biz',
+    id: 'vifacerollkeys1',
+  }];
 
-  describe('getFakeData', () => {
-    it('should return the fake data from JSON', () => {
-      const result = service.getMovieList();
-      expect(result).toEqual(fakeData);
+  /** mock DB Connection
+   * @arg {String} arg query of data
+   * @return {Promise} fake data
+  */
+  class DBFake {
+    query = (arg) => {
+      return Promise.resolve({
+        records: fakeRecords,
+      });
+    };
+  }
+
+  describe('getMovieList', () => {
+    beforeEach(() => {
+      service.db = new DBFake();
+    });
+    it('should return the fake data from JSON', async () => {
+      jest.spyOn(service.db, 'query');
+      let result;
+
+      try {
+        result = await service.getMovieList();
+      } catch (e) {
+        expect(true).toBe(false);
+      }
+
+      expect(service.db.query).toBeCalledTimes(1);
+      expect(service.db.query)
+          .toHaveBeenCalledWith('select id, title, poster from movies');
+      expect(result).toEqual(fakeRecords);
+    });
+    it('should pass errors up and out', async () => {
+      jest.spyOn(service.db, 'query')
+          .mockReturnValue(Promise.reject(new Error('nope')));
+
+      let result;
+
+      try {
+        await service.getMovieList();
+      } catch (e) {
+        result = e;
+      }
+
+      expect(service.db.query).toBeCalledTimes(1);
+      expect(service.db.query)
+          .toHaveBeenCalledWith('select id, title, poster from movies');
+      expect(result.message).toEqual('nope');
     });
   });
 });
